4a5ada71eccd61deb2e6b6bbc0af31892bcb2eca
Daan de Graaf <d.degraaf@dictu.nl>
2025-03-05 19:30:14 +0100

    #11 Add multibrowser/platform support

--- js/search.js
+++ js/search.js
@@ -5,13 +5,19 @@ Array.from(searchBars).forEach(function (searchBar) {
     var urlWithPlaceholder = searchBar.dataset.url;
     var searchTerm = this.value;
 
-    if (e.metaKey && e.keyCode === 13) {
+    if (isTargetBlank(e) && e.keyCode === 13) {
       search(urlWithPlaceholder, searchTerm, "_blank")
     } else if (e.keyCode === 13) {
       search(urlWithPlaceholder, searchTerm, "_self")
     }
   })
 })
+
+function isTargetBlank(e) {
+  var keyCode = e.keyCode;
+  return e.metaKey || keyCode === 93 || keyCode === 91 || keyCode === 224
+    || keyCode === 17;
+}
 
 function search(url, term, target) {
   var searchUrl = url.replace("[search-term]", encodeURIComponent(term));
