20c65e3a0336d8ded92d739a6bb20591615e2afd
Daan de Graaf <d.degraaf@dictu.nl>
2025-03-07 07:19:22 +0100

    #10 Reformat code

--- js/clock.js
+++ js/clock.js
@@ -12,29 +12,30 @@ const clockElement = document.getElementById('clock');
  * Return a string containing the formatted current date and time.
  */
 function getDateTime() {
-    let day = dateTime.getDate();
-    let month = dateTime.getMonth() + 1;
-    let hour = dateTime.getHours();
-    let minutes = dateTime.getMinutes();
-    let seconds = dateTime.getSeconds();
+  let day = dateTime.getDate();
+  let month = dateTime.getMonth() + 1;
+  let hour = dateTime.getHours();
+  let minutes = dateTime.getMinutes();
+  let seconds = dateTime.getSeconds();
 
-    if (hour < 0) {
-        hour = 24 + hour;
-    }
+  if (hour < 0) {
+    hour = 24 + hour;
+  }
 
-    let date = (day < 10 ? '0' + day : day) + '-' + (month < 10 ? '0' + month : month) + '-' + dateTime.getFullYear();
-    let time = (hour < 10 ? '0' + hour : hour) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds);
+  let date = (day < 10 ? '0' + day : day) + '-' + (month < 10 ? '0' + month
+    : month) + '-' + dateTime.getFullYear();
+  let time = (hour < 10 ? '0' + hour : hour) + ':' + (minutes < 10 ? '0'
+    + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds);
 
-    return date + '\n' + time;
+  return date + '\n' + time;
 }
 
 function setClock() {
-    clockElement.innerText = getDateTime();
+  clockElement.innerText = getDateTime();
 }
-
 
 setClock();
 
 setInterval(() => {
-    setClock();
+  setClock();
 }, clockInterval);
