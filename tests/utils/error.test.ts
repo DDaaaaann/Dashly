0f102135b51295a605c8709a2a33c293c970ad69
Daan de Graaf <d.degraaf@dictu.nl>
2025-03-13 21:46:30 +0100

    #23 Unit test error.ts

---
+++ tests/utils/error.test.ts
@@ -0,0 +1,43 @@
+import {getErrorMessage} from '../../src/utils/error';
+
+describe('getErrorMessage', () => {
+  it('should return the error message when the error is an instance of Error', () => {
+    const error = new Error('Something went wrong');
+
+    const result = getErrorMessage(error);
+
+    expect(result).toBe('Something went wrong');
+  });
+
+  it('should return the string representation of the error when the error is not an instance of Error', () => {
+    const error = 'This is a string error';
+
+    const result = getErrorMessage(error);
+
+    expect(result).toBe('This is a string error');
+  });
+
+  it('should return the string representation of an unknown error', () => {
+    const error = 123;
+
+    const result = getErrorMessage(error);
+
+    expect(result).toBe('123');
+  });
+
+  it('should return the string representation of null error', () => {
+    const error = null;
+
+    const result = getErrorMessage(error);
+
+    expect(result).toBe('null');
+  });
+
+  it('should return the string representation of undefined error', () => {
+    const error = undefined;
+
+    const result = getErrorMessage(error);
+
+    expect(result).toBe('undefined');
+  });
+});
